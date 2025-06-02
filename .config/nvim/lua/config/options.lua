250eed3f05e10831749487ee1f9327926cc69577
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-06-02 21:32:42 +0100

    fix: save to clipboard when yanking
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,14 +1,27 @@
--- Tabs & Spacing
+-- tabs & spacing
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
--- Line Numbers
+-- line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
--- Lines
+-- lines
 vim.opt.wrap = false
 vim.opt.linebreak = true
--- Improved Searching
+-- improved searching
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+-- clipboard
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy --type text/plain",
+    ["*"] = "wl-copy --type text/plain --primary",
+  },
+  paste = {
+    ["+"] = "wl-paste --no-newline",
+    ["*"] = "wl-paste --no-newline --primary",
+  },
+  cache_enabled = 1,
+}
