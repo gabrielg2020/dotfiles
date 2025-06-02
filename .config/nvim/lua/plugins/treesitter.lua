7e95122afaa07f4f815022ea1ced8473a8613285
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2025-06-02 22:15:49 +0100

    fix: treesitter
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,23 +5,35 @@ return {
     lazy = false,
     build = ":TSUpdate",
     opts = {
-      "bash",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "printf",
-      "python",
-      "query",
-      "regex",
-      "templ",
-      "toml",
-      "typescript",
-      "vim",
-      "xml",
-      "yaml",
-    }
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "printf",
+        "python",
+        "query",
+        "regex",
+        "templ",
+        "toml",
+        "typescript",
+        "vim",
+        "xml",
+        "yaml",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+    },
+    -- note: need to use .configs setup function
+    config = function (_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end
   }
 }
