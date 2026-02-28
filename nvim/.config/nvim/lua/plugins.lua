9d9f13fb9efd7acc6e8505744f73df01c5cdff9f
Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>
2026-02-28 02:00:48 +0000

    chore: update
    Signed-off-by: Gabriel Guimaraes <80750139+gabrielg2020@users.noreply.github.com>

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -8,6 +8,7 @@ vim.pack.add({
 	{ src = gh('echasnovski/mini.pairs') },
 	{ src = gh('echasnovski/mini.comment') },
 	{ src = gh('echasnovski/mini.diff') },
+	{ src = gh('echasnovski/mini.indentscope') },
 	{ src = gh('mason-org/mason.nvim') },
 	{ src = gh('mason-org/mason-lspconfig.nvim') },
 	{ src = gh('saghen/blink.cmp') },
@@ -40,14 +41,47 @@ for _, server_name in ipairs(mason.get_installed_servers()) do
 	vim.lsp.enable(server_name)
 end
 
+-- helper to determine if hidden files should be shown
+local is_exception = function(name)
+	local exceptions = { '.github', '.env' }
+	for _, exception in ipairs(exceptions) do
+		if name == exception or vim.startswith(name, exception) then
+			return true
+		end
+	end
+	return false
+end
+
 -- file explorer
-require 'oil'.setup()
+require 'oil'.setup({
+	view_options = {
+		is_hidden_file = function(name, bufnr)
+			return not is_exception(name) and vim.startswith(name, '.')
+		end,
+	}
+})
 
 -- minis
-require 'mini.pick'.setup()    -- selection picker
-require 'mini.comment'.setup() -- block commenting
-require 'mini.pairs'.setup()   -- autopairs
-require 'mini.diff'.setup()    -- diff lines
+local pick = require 'mini.pick'
+pick.setup()        -- selection picker
+
+-- override Pick files to show .github and .env files
+pick.registry.files = function(local_opts)
+	local_opts = vim.tbl_deep_extend('force', { tool = 'fd' }, local_opts or {})
+	local cli_opts = {
+		'--color=never', '--type=f',
+		'--hidden',  -- show hidden files
+		'--exclude=.git',  -- but exclude .git
+		'--exclude=node_modules',  -- exclude node_modules
+		'--exclude=__pycache__',  -- exclude python cache
+		'--exclude=venv',  -- exclude python venv
+	}
+	return pick.builtin.cli({ command = { 'fd', unpack(cli_opts) } }, local_opts)
+end
+require 'mini.comment'.setup()     -- block commenting
+require 'mini.pairs'.setup()       -- autopairs
+require 'mini.diff'.setup()        -- diff lines
+require 'mini.indentscope'.setup() -- indent scoping
 
 -- theme
 vim.cmd('colorscheme vague')
